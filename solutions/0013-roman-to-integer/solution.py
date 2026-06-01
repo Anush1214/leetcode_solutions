49fc7eb7d02691608f6d7c1634e134e57acec583
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-01 10:39:25 +0000

    Sync LeetCode submission Runtime - 11 ms (46.74%), Memory - 12.5 MB (21.25%)

---
+++ solutions/0013-roman-to-integer/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total=0
+        n=len(s)
+        for i in range(n):
+            if i<n-1 and d[s[i]]<d[s[i+1]]:
+                total-=d[s[i]]
+            else:
+                total+=d[s[i]]
+        return total
+
