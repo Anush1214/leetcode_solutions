3015eb0fbcce0ee7c630917d9143dac00a64b901
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-02 06:33:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (89.82%)

---
+++ solutions/1635-number-of-good-pairs/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        pair=0
+
+        # for i in nums:
+        #     if i in count:
+        #         pair+=count[i]
+        #         count[i]+=1
+        #     else:
+        #         count[i]=1
+        # return pair
+
+        for i in nums:
+            d[i]=d.get(i,0)+1
+
+        for i in d:
+            if d[i]>1:
+                pair+=((d[i]-1)*d[i])//2
+        return pair
