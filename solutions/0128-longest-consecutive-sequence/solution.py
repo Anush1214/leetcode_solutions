c2636ad7d18764d3c6f50aa41f7aa485fa0327af
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-03 04:12:44 +0000

    Sync LeetCode submission Runtime - 64 ms (28.79%), Memory - 26.9 MB (46.24%)

---
+++ solutions/0128-longest-consecutive-sequence/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s=set(nums)
+        longest=0
+        for num in s:
+            if num-1 not in s:
+                current=num
+                count=1
+                while current+1 in s:
+                    current +=1
+                    count+=1
+                longest=max(longest,count)
+        return longest
