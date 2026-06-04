37334631f7b3dabc7da748151fc2e7cb109509b2
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-04 04:30:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (28.79%)

--- solutions/0062-unique-paths/solution.py
+++ solutions/0062-unique-paths/solution.py
@@ -1,5 +1,10 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
         dp = [1] * n  # first row
         
         for _ in range(1, m):
@@ -7,4 +12,3 @@ class Solution:
                 dp[j] += dp[j - 1]
         
         return dp[-1]
-        
