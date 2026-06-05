d900f4cc2c48a8e4a535cb288a49c0f9ab87fb96
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-05 04:30:34 +0000

    Sync LeetCode submission Runtime - 288 ms (39.58%), Memory - 13.9 MB (83.72%)

---
+++ solutions/2470-removing-stars-from-a-string/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        st=[]
+        for ch in s:
+            if st and ch=="*":
+                st.pop()
+            else:
+                st.append(ch)
+        return "".join(st)
