9f20b3905637da6f91c64fa5ae78d2a954b188b2
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-06 05:07:43 +0000

    Sync LeetCode submission Runtime - 10 ms (45.95%), Memory - 19.3 MB (66.41%)

---
+++ solutions/1298-reverse-substrings-between-each-pair-of-parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack=[]
+        for ch in s:
+            if ch==")":
+                temp=[]
+                while stack and stack[-1]!="(":
+                    temp.append(stack.pop())
+                stack.pop()
+                stack.extend(temp)
+            else:
+                stack.append(ch)
+        return "".join(stack)
