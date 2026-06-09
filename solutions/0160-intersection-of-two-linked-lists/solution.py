4231a9afd74c596524be466c80b36cae0a00bfda
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-09 08:56:51 +0000

    Sync LeetCode submission Runtime - 121 ms (17.41%), Memory - 38.1 MB (30.46%)

---
+++ solutions/0160-intersection-of-two-linked-lists/solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        t1=headA
+        t2=headB
+        if not t1 or not t2:
+            return None
+        while t1!=t2:
+            if t1:
+                t1=t1.next
+            else:
+                t1=headB
+            if t2:
+                t2=t2.next
+            else:
+                t2=headA
+        return t1 
+
