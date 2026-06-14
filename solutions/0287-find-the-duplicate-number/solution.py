c6a361686e16020a0cda8c6e55ba5cc281e40461
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-06-14 16:16:50 +0000

    Sync LeetCode submission Runtime - 181 ms (5.00%), Memory - 33.6 MB (29.31%)

---
+++ solutions/0287-find-the-duplicate-number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        left=1
+        right=len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            count=0
+            for i in nums:
+                if i<=mid:
+                    count+=1
+            
+            if count>mid:
+                right=mid
+            else:
+                left=mid+1
+        return left
