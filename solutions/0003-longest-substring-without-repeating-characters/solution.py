d400b2361a798b35e5541e9d257b3d83b32a1c9d
Anush C Rao <142653927+Anush1214@users.noreply.github.com>
2026-09-07 11:39:57 +0000

    Sync LeetCode submission Runtime - 239 ms (21.89%), Memory - 20 MB (24.87%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.py
+++ solutions/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,14 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        index={}
-        maxl=0
-        left=0
+        # n=len(s)
+        # maxlen=0
+        # for i in range(n):
+        #     hash_set=[0]*256
+        #     for j in range(i,n):
+        #         if hash_set[ord(s[j])]==1:
+        #             break
+        #         hash_set[ord(s[j])]=1
+        #         curr=j-i+1
+        #         maxlen=max(maxlen,curr)
+        # return maxlen
 
-        for right in range(len(s)):
-            char=s[right]
-            if char in index and index[char]>=left:
-                left=index[char]+1
-            index[char]=right
-            curr=right-left+1
-            maxl=max(maxl,curr)
-        return maxl
+        n=len(s)
+        hashlen=256
+        hash=[-1]*hashlen
+
+        for i in range(hashlen):
+            hash[i]=-1
+        l,r,maxlen=0,0,0
+        while r<n:
+            if hash[ord(s[r])]!=-1:
+                l=max(hash[ord(s[r])]+1,l)
+            curr=r-l+1
+            maxlen=max(maxlen,curr)
+            hash[ord(s[r])]=r
+            r+=1
+        return maxlen
